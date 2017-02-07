c0a9dd5c9dd103c9963e7c122f2d81d74464e7d2
unknown <shivam.bhatnagar@NST-TRAINEE2.svam.com>
2017-02-07 16:42:44 +0530

    Code Setup

---
+++ Index.js
@@ -0,0 +1,57 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var express = require("express");
+var path = require('path');
+var Array = require('node-array');
+var redis = require('redis');
+var client = redis.createClient();
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+//Redis Connections
+client.on('connect', function() {
+	console.log('connected');
+});
+
+// load static file
+app.use(express.static(path.join(__dirname, 'Public')));
+
+//routes and sends to the views
+app.get('/', function(req, res) {
+	res.sendFile('Views/Index.html', { /* var name = req.param('name');*/
+		root : __dirname
+	});
+	/*defining global varible (name)it is getting the name 
+	from rhe cookies which we set at the time user entered their name*/
+	name = req.cookies.name;
+    console.log(name);
+});
+//Setting Connection
+io.on('connection', function(socket) {
+	//got the user id form socket this is the unique id for each and every connection
+	var id = socket.id;
+	socket.on('chat message', function(msg) {
+		var str = '{ "name":" ' + global.name + ' " ,"id":"' + id
+				+ '" , "message":"' + msg + '"  }';
+
+		var jsonObj = JSON.parse(str);
+		//pushed the message typed by the user                                              
+		client.rpush([ 'messages', JSON.stringify(jsonObj) ]);
+
+		//Getting last pushed message from redis
+		client.lrange('messages', -1, -1, function(err, reply) {
+			io.emit('chat message', reply);
+		});
+	});
+	//Getting all the messages from redis
+	client.lrange('messages', 0, -1, function(err, Allreply) {
+
+		io.emit('all messages', Allreply);
+	});
+});
+
+//Node work on specified port
+http.listen(3000, function() {
+	console.log('bhut bdia');
+});
---
+++ Public/JS/Main.js
@@ -0,0 +1,87 @@
+//Initilize socket.io connection	
+var socket = io(); 
+	var i;
+	
+
+// GET the user's name and set it in the cookies
+function setName()
+	{	
+		var name= Cookies.get('name');
+		if (!name || name == null)
+		{
+			var name= window.prompt('What is you name?');
+		    Cookies.set('name',name,1);
+		}
+		socket.emit('chat message',name);
+		$('#messageBox').focus();
+		
+		return name;
+} 
+
+function getName(){
+	return Cookies.get('name');
+}
+
+//Takes time as a parameter and convert it into the string
+function getTime(timestamp)
+{
+	var t,h,m,s;
+	t= new Date(timestamp);
+	h=t.getHours();
+	m=t.getMinutes();
+	s=t.getSeconds();
+	
+	return String(h + ':' + m + ':' + s) ;
+}	
+
+	/* Emit the message sended by the user */
+	$('form').submit(function() {
+		
+	 // if input is empty or white space do not send message                                                   
+	    if ($('#messageBox').val()
+	    .match(/^[\s]*$/) !== null) {
+	      $('#messageBox').val('');
+	      $('#messageBox').attr('placeholder', 'please enter your message here');
+	      $('#messageBox').focus();
+	      return false;
+	    }
+	    if (!Cookies.get('name') || Cookies.get('name').length < 1
+	    	      || Cookies.get('name') === 'null') {
+	    	      getName();
+	    	    } 
+	    else{
+		socket.emit('chat message', $('#messageBox').val());
+		$('#messageBox').val('');
+		 $('#messageBox').attr('placeholder', '');
+	    }
+		return false;
+	});
+	
+	
+	/* 
+	 * showing all the messages
+	 * poped from the redis
+	 */	
+	socket.on('all messages', function(Allreply) {	
+		$('#messages').empty();
+	          var name=getName();
+		for (i in Allreply) {
+			var msg = name + ":" + Allreply[i];
+			$('#messages').append($('<li>').text(msg));
+		}
+	});
+
+
+	/*
+	 * showing last inserted messages so that user need not to refresh the page
+	 * every time the new message sended
+	 */
+	  /*var reply =JSON.parse(reply);  var name=reply.name ;*/
+	
+	socket.on('chat message', function(reply) {                                              
+		var name=getName();
+		for (i in reply) {
+			var msg = name + ":" + reply[i];
+			$('#messages').append($('<li>').text(msg));
+		}
+	});
